e0de6cf02887b9a5e2a2d255bbbf9c5c74fc1ae4
unknown <acemiranda13@att.net>
2021-07-17 19:02:46 -0700

    updated server.js

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.use(express.static(path.resolve(__dirname, 'dist')));
 
 app.get('*', (req, res) => {
   //   // res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
+  res.sendFile(path.resolve(__dirname, 'dist/index.html'));
 });
 // }
 
