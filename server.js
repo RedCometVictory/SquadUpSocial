f05e5d9c16e88e10463e27a53b8e06d2484f2953
unknown <acemiranda13@att.net>
2021-07-17 17:37:48 -0700

    updated server.js

--- server.js
+++ server.js
@@ -42,14 +42,14 @@ app.use('/api/users', usersRoutes); // '/users' = '/'
 // Serve static assets in production
 // if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static(path.resolve(__dirname, 'dist')));
+app.use(express.static(path.resolve(__dirname, 'dist')));
   // app.use(express.static('./dist'));
   // app.use(express.static('client/build'));
 
-  app.get('*', (req, res) => {
+app.get('*', (req, res) => {
   //   // res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
-  });
+  res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
+});
 // }
 
 // 404 -- not needed if 404 page exists
